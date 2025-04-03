97dc57140d7e4abb2c61641e322d7b9999c0d8db
samupietila <samupie@metropolia.fi>
2025-04-03 22:38:37 +0300

    Drawing Canvas now possible

--- app/src/main/java/com/example/mobiilisovellusprojekti/MainActivity.kt
+++ app/src/main/java/com/example/mobiilisovellusprojekti/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mobiilisovellusprojekti.ViewModels.DrawingAction
+import com.example.mobiilisovellusprojekti.ViewModels.DrawingViewModel
+import com.example.mobiilisovellusprojekti.ViewModels.allColors
+import com.example.mobiilisovellusprojekti.screens.screens.CanvasControls
+import com.example.mobiilisovellusprojekti.screens.screens.DrawingCanvas
 import com.example.mobiilisovellusprojekti.ui.theme.MobiilisovellusProjektiTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +31,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             MobiilisovellusProjektiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val viewModel = viewModel<DrawingViewModel>()
+                    val state by viewModel.state.collectAsStateWithLifecycle()
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        DrawingCanvas(
+                            paths = state.paths,
+                            currentPath = state.currentPath,
+                            onAction = viewModel::onAction,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .weight(1f)
+                        )
+                        CanvasControls(
+                            selectedColor = state.selectedColor,
+                            colors = allColors,
+                            onSelectColor = {
+                                viewModel.onAction(DrawingAction.OnSelectColor(it))
+                            },
+                            onClearCanvas = {
+                                viewModel.onAction((DrawingAction.OnClearCanvasClick))
+                            },
+
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobiilisovellusProjektiTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/mobiilisovellusprojekti/ViewModels/DrawingViewModel.kt
@@ -0,0 +1,94 @@
+package com.example.mobiilisovellusprojekti.ViewModels
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class  DrawingState(
+    val selectedColor: Color = Color.Black,
+    val currentPath: PathData? = null,
+    val paths: List<PathData> = emptyList()
+)
+
+val allColors = listOf(
+    Color.Black,
+    Color.Red,
+    Color.Blue,
+    Color.Yellow,
+    Color.Green,
+    Color.White,
+)
+
+data class PathData(
+    val id: String,
+    val color: Color,
+    val path: List<Offset>
+)
+
+sealed interface DrawingAction {
+    data object OnNewPathStart: DrawingAction
+    data class OnDraw(val offset: Offset): DrawingAction
+    data object OnPathEnd: DrawingAction
+    data class OnSelectColor(val color: Color): DrawingAction
+    data object OnClearCanvasClick: DrawingAction
+}
+
+class DrawingViewModel: ViewModel() {
+    private val _state = MutableStateFlow(DrawingState())
+    val state = _state.asStateFlow()
+
+    fun onAction(action: DrawingAction) {
+        when (action) {
+            DrawingAction.OnClearCanvasClick -> onClearCanvas()
+            is DrawingAction.OnDraw -> onDraw(action.offset)
+            DrawingAction.OnNewPathStart -> onNewPathStart()
+            DrawingAction.OnPathEnd -> onPathEnd()
+            is DrawingAction.OnSelectColor -> onSelectColor(action.color)
+        }
+    }
+
+    private fun onClearCanvas() {
+        _state.update {
+            it.copy(
+                currentPath = null,
+                paths = emptyList()
+            )
+        }
+    }
+
+    private fun onSelectColor(color: Color) {
+        _state.update { it.copy(
+            selectedColor = color
+        ) }
+    }
+
+    private fun onDraw(offset: Offset) {
+        val currentPath = state.value.currentPath ?: return
+        _state.update { it.copy(
+            currentPath = currentPath.copy(
+                path = currentPath.path + offset
+            )
+        ) }
+    }
+
+    private fun onNewPathStart() {
+        _state.update { it.copy(
+            currentPath = PathData(
+                id = System.currentTimeMillis().toString(),
+                color = it.selectedColor,
+                path = emptyList()
+            )
+        ) }
+    }
+
+    private fun onPathEnd() {
+        val currentPathData = state.value.currentPath ?: return
+        _state.update { it.copy(
+            currentPath = null,
+            paths = it.paths + currentPathData
+        ) }
+    }
+}
