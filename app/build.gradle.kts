a2f45f871c23039eb3c18de3cfa4ad97a6a46168
samupietila <samupie@metropolia.fi>
2025-04-16 12:17:12 +0300

    Tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
 
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
